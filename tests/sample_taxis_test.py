7994842c7682624a6b3a041633d4e2b2b1b34297
Drew Furgiuele <drew@dublindata.net>
2026-02-08 21:12:14 -0500

    resolving serverless issue

--- tests/sample_taxis_test.py
+++
@@ -1,8 +0,0 @@
-from databricks.sdk.runtime import spark
-from pyspark.sql import DataFrame
-from export_data_by_table_with_tags import taxis
-
-
-def test_find_all_taxis():
-    results = taxis.find_all_taxis()
-    assert results.count() > 5
